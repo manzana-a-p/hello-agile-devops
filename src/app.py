15dc1a2ba5add999f89269799d5a43f915886374
StackArtisan <122510797+manzana-a-p@users.noreply.github.com>
2025-08-28 21:16:27 +0800

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,5 +1,7 @@
 def greet(name: str) -> str:
     return f"Hello, {name}! Welcome to CI/CD with Agile DevOps."
 
+
 if __name__ == "__main__":
+    # This runs only when you call: py src/app.py
     print(greet("LeBonCyl"))
