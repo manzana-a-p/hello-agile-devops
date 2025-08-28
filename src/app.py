6d14890eaa7af149e953e8dd230ab73c926d53bd
StackArtisan <122510797+manzana-a-p@users.noreply.github.com>
2025-08-28 21:25:35 +0800

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,8 +1,7 @@
 def greet(name: str) -> str:
-    return f"Hello, {name}! Welcome to CI/CD with Agile DevOps."
+    return f"Hello, {name}! Welcome to Agile DevOps."
 
 
 if __name__ == "__main__":
-    # This block runs only when you execute: py src/app.py
     print("DEBUG: app.py is running...")
     print(greet("LeBonCyl"))
